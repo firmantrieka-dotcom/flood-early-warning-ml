c339f4689aae90c879c6e8be000fdd9f242f3ca2
firmantrieka-dotcom <firmantrieka@students.polmed.ac.id>
2026-06-25 17:08:25 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,26 @@
-from flask import Flask
+from flask import Flask, jsonify
+from predict import prediksi_dan_kirim
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Flood Early Warning ML is Running on Railway"
+    return "Flood Early Warning ML Railway is Running"
+
+@app.route("/predict")
+def predict():
+    try:
+        hasil = prediksi_dan_kirim()
+        return jsonify({
+            "status": "success",
+            "message": "Prediksi berhasil dikirim ke Firebase",
+            "data": hasil
+        })
+    except Exception as e:
+        return jsonify({
+            "status": "error",
+            "message": str(e)
+        }), 500
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
