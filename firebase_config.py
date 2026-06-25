81cbbd6d12f7471a2f2f767ca91afab3cfcad50a
firmantrieka-dotcom <firmantrieka@students.polmed.ac.id>
2026-06-25 17:46:14 +0700

    Update firebase_config.py

--- firebase_config.py
+++ firebase_config.py
@@ -7,22 +7,22 @@ def init_firebase():
     if firebase_admin._apps:
         return
 
+    firebase_json = os.environ.get("FIREBASE_CREDENTIALS_JSON")
     database_url = os.environ.get("FIREBASE_DATABASE_URL")
-    credentials_json = os.environ.get("FIREBASE_CREDENTIALS_JSON")
+
+    if not firebase_json:
+        raise RuntimeError("FIREBASE_CREDENTIALS_JSON belum diatur di Railway Variables")
 
     if not database_url:
         raise RuntimeError("FIREBASE_DATABASE_URL belum diatur di Railway Variables")
 
-    if not credentials_json:
-        raise RuntimeError("FIREBASE_CREDENTIALS_JSON belum diatur di Railway Variables")
-
-    cred_dict = json.loads(credentials_json)
+    cred_dict = json.loads(firebase_json)
     cred = credentials.Certificate(cred_dict)
 
     firebase_admin.initialize_app(cred, {
         "databaseURL": database_url
     })
 
-def get_db():
+def firebase_db():
     init_firebase()
     return db
